7a43a72defcc0b37aa2cf1a28b9a932d41146f41
xiaoxin <xiaoxin123/>
2021-03-04 15:36:15 +0800

    用@Value来读自定义配置项，没有值配置默认值。

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 wiki project
 
 在线生成启动图案：http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+properties和yml文件互转：https://www.toyaml.com/index.html
 
 idea:https://www.imooc.com/video/20243
--- src/main/java/com/xiaoxin/wiki/controller/TestController.java
+++ src/main/java/com/xiaoxin/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.xiaoxin.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.name:jhang}")
+    private String name;
+
     @GetMapping("/test")
     public String test() {
-        return "hello world";
+        return "hello " + name ;
     }
 }
