3f9766dd239725b0dcd3052b9d9ef896d7b47e58
xiaoxin <xiaoxin123/>
2021-03-04 15:17:29 +0800

    修改配置类路径

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my_wiki
 wiki project
+
+在线生成启动图案：http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+
+idea:https://www.imooc.com/video/20243
---
+++ src/main/java/com/xiaoxin/wiki/config/WikiApplication.java
@@ -0,0 +1,23 @@
+package com.xiaoxin.wiki.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@ComponentScan(value = "com.xiaoxin.wiki")
+@SpringBootApplication
+public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
