d57d7da77a0fe2a78b0d5f04423c83018f04a96c
xiaoxin <xiaoxin123/>
2021-03-04 17:21:34 +0800

    集成 generator 代码生成器，生成demo测试代码

---
+++ src/main/java/com/xiaoxin/wiki/domain/Demo.java
@@ -0,0 +1,35 @@
+package com.xiaoxin.wiki.domain;
+
+public class Demo {
+    private Integer id;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", content=").append(content);
+        sb.append("]");
+        return sb.toString();
+    }
+}
